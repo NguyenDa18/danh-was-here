f9432d9f19650e4ee7e065fba22896de275799b3
Danh Nguyen <danhnguyen267@gmail.com>
2020-07-06 21:53:27 -0700

    Updated contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -20,22 +20,22 @@ const ContactPage = ({
           <p>Let me help you kick start your next project &rarr;</p>
         </div>
         <div>
-          <form className="form-container" netlify>
+          <form className="contact" method="POST" netlify>
             <div>
-              <label htmlFor="w3lName">Name</label>
-              <input type="text" name="w3lName" id="w3lName" />
+              <label htmlFor="name">Name</label>
+              <input type="text" name="name" id="name" />
             </div>
             <div>
-              <label htmlFor="w3lSender">Email</label>
-              <input type="email" name="w3lSender" id="w3lSender" />
+              <label htmlFor="sender">Email</label>
+              <input type="email" name="sender" id="sender" />
             </div>
             <div>
-              <label htmlFor="w3lSubject">Subject</label>
-              <input type="text" name="w3lSubject" id="w3lSubject" />
+              <label htmlFor="subject">Subject</label>
+              <input type="text" name="subject" id="subject" />
             </div>
             <div>
-              <label htmlFor="w3lMessage">Message</label>
-              <textarea name="w3lMessage" id="w3lMessage"></textarea>
+              <label htmlFor="message">Message</label>
+              <textarea name="message" id="message"></textarea>
             </div>
             <div style={{ display: "flex", justifyContent: "flex-end" }}>
               <input type="submit" className="button -primary" style={{ marginRight: 0 }} />
